9a0ddc4126e7025ae7a6ab0546693ee370496790
rafinsripon <rafinsripon@gmail.com>
2022-11-24 17:33:22 +0600

    full setup install and categories and category api done

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 5000
+
+require('dotenv').config()
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0mxdn2v.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const usersCollection = client.db('usecarmarket').collection('users')
+        const categoriesCollection = client.db('usecarmarket').collection('categories')
+        const categoryCollection = client.db('usecarmarket').collection('category')
+
+        //get categories
+        app.get('/categories', async(req, res) => {
+            const query = {};
+            const categories = await categoriesCollection.find(query).toArray()
+            res.send(categories);
+        })
+
+
+        app.get('/category', async(req, res) => {
+          const query = {};
+          const category = await categoryCollection.find(query).toArray()
+          res.send(category)
+        })
+
+        //get single data
+        app.get('/category/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {category_id:(id)}
+            const category = await categoryCollection.find(query).toArray();
+            res.send(category);
+        })
+
+        app.post('/users', async(req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user)
+            res.send(result);
+        })
+  
+        console.log('Database Connected yes...')
+    } 
+    finally {
+
+    }
+  }
+  
+  run().catch(err => console.error(err))
+  
+  app.get('/', (req, res) => {
+    res.send('Server is running...')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Server is running...on ${port}`)
+  })
+  
+
+
